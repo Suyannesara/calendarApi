ca112c8f6bf163bcf9a2a6e5d6a0c3ce7ce5f9e6
Suyannesara <suyannesara17@gmail.com>
2023-03-12 19:32:52 -0300

    ⚡feat: calc percent of activities in lists

--- tasksStats.py
+++ tasksStats.py
@@ -10,33 +10,34 @@ server = Task().init()
 flagsList = listTasksList(server)
 # print(flagsList)
 
-tasksDf = pd.DataFrame()
-tasks = []
+tasks = tasksLen = {}
+totalTasks = 0
 
 for flag in flagsList:
-    tasksDf[flag['title']] = ''
-
     response = server.tasks().list(
         tasklist=flag['id'],
-        maxResults=1,
+        maxResults=20,
         showDeleted=False,
         # dueMin='2023-03-11T00:00:00.000Z'
     ).execute()
     items = response.get('items')
-    # tasks.append({flag['title']: items['id']})
-    
-    # if items:
-    #     tasksDf[flag['title']] = len(items)
 
-# code = len(tasksDf['CODE'])
-print(items)
-# tasks = response.get('items')
+    totalTasks += len(items)
+    # Group items in a object according to its flag 
+    tasks[flag['title']] = items
+    # Group items LENGTH in a object according to its flag 
+    tasksLen[flag['title']] = len(items)
 
+print(totalTasks)
 
 # RETORNAR QUANTAS TAREFAS DE CADA LISTA FORAM FEITAS
 
-
-
 # CALCULAR PORCENTAGEM IMPACTO DA LISTA X NO TOTAL DE ATIVIDADES
+# X% das suas atividades sao advindas da lista Y 
+percentLists = {}
+for key, value in tasks.items():
+    percentLists[key] = round((value/totalTasks)*100)
+
+    print(f"{percentLists[key]}% of your tasks comes from flag: {key}")
 
 
