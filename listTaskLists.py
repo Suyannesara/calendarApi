1af0348ed45094c4e4c5474aad2cc544faade6df
Suyannesara <suyannesara17@gmail.com>
2023-03-11 22:03:17 -0300

    ⚡ feat: listTasksList function

---
+++ listTaskLists.py
@@ -0,0 +1,19 @@
+from tasksServer import Task
+# RETURNS A LIST OF TASKLISTS 
+
+def listTasksList(server):
+    response = server.tasklists().list().execute()
+
+    tasksList = response.get('items')
+    nextPageToken = response.get('nextPageToken')
+
+    while nextPageToken:
+        response = server.tasklists().list(
+            maxResults=30,
+            pageToken=nextPageToken
+        )
+        tasksList.extend(response.get('items'))
+        nextPageToken = response.get('nextPageToken')
+
+    return tasksList
+
