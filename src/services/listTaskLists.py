1283717cb270c0638876801fe812424609508a90
Suyannesara <suyannesara17@gmail.com>
2023-03-21 20:20:44 -0300

    ♻️ refact: divite list in a service

--- src/services/listTaskLists.py
+++ src/services/listTaskLists.py
@@ -1,5 +1,8 @@
-# RETURNS A LIST OF TASKLISTS 
+from ..servers.Google import convert_to_RFC_datetime
+import pandas as pd
 
+
+# RETURNS A LIST OF TASKLISTS 
 def listTasksList(server):
     response = server.tasklists().list().execute()
 
@@ -16,3 +19,69 @@ def listTasksList(server):
 
     return tasksList
 
+def listTasksInList(server):
+    # PEGAR TASKS DE UMA LISTA X
+    flagsList = listTasksList(server)
+
+    # INITAL RESPONSE 
+    
+    response = server.tasks().list(
+        tasklist=flagsList[0]['id'],
+        maxResults=100,
+        # showDeleted=True,
+        showCompleted=True,
+        # completedMax=convert_to_RFC_datetime(2023, 2, 10),
+    ).execute()
+
+    items = response.get('items')
+    nextPageToken = response.get('nextPageToken')
+
+    while nextPageToken:
+        for flag in flagsList:
+            response = server.tasks().list(
+                tasklist=flag['id'],
+                maxResults=100,
+                showDeleted=True,
+                showCompleted=True,
+                # dueMax=convert_to_RFC_datetime(2022, 2, 10),
+                pageToken=nextPageToken
+            ).execute()
+            items = response.get('items')
+            nextPageToken = response.get('nextPageToken')
+    print(pd.DataFrame(items))
+
+
+    # tasksNextPageToken = response.get('nextPageToken')
+
+    # while nextPageToken:
+    #     print("tem taskss")
+    #     nextPageToken = response.get('nextPageToken')
+
+    # return tasksList
+
+
+
+    # tasksLen = {}
+    # tasks = {}
+    # totalTasks = 0
+
+    # for flag in flagsList:
+    #     response = server.tasks().list(
+    #         tasklist=flag['id'],
+    #         maxResults=100,
+    #         showDeleted=False,
+    #         showCompleted=True,
+    #         # completedMax='2023-02-11T00:00:00.000Z'
+    #     ).execute()
+    #     items = response.get('items')
+
+    #     totalTasks += len(items)
+    #     # Group items in a object according to its flag 
+    #     tasks[flag['title']] = items
+    #     # Group items LENGTH in a object according to its flag 
+    #     tasksLen[flag['title']] = len(items)
+
+    # print(totalTasks)
+
+
+# listTasksInList()
--- tasksStats.py
+++ tasksStats.py
@@ -1,53 +1,61 @@
 from src.servers.Google import convert_to_RFC_datetime
-from src.services.listTaskLists import listTasksList
+from src.services.listTaskLists import listTasksList, listTasksInList
 from src.servers.tasksServer import Task
 import pandas as pd
 
 server = Task().init()
 
 # PEGAR TASKS DE UMA LISTA X
-flagsList = listTasksList(server)
+# flagsList = listTasksList()
 
 tasksLen = {}
 tasks = {}
 totalTasks = 0
 
-for flag in flagsList:
-    response = server.tasks().list(
-        tasklist=flag['id'],
-        maxResults=20,
-        showDeleted=False,
-        dueMax='2023-02-11T00:00:00.000Z'
-    ).execute()
-    items = response.get('items')
 
-    totalTasks += len(items)
-    # Group items in a object according to its flag 
-    tasks[flag['title']] = items
-    # Group items LENGTH in a object according to its flag 
-    tasksLen[flag['title']] = len(items)
-
-print(totalTasks)
-
-# RETORNAR QUANTAS TAREFAS DE CADA LISTA FORAM FEITAS
-
-# CALCULAR PORCENTAGEM IMPACTO DA LISTA X NO TOTAL DE ATIVIDADES
-# X% das suas atividades sao advindas da lista Y 
-percentLists = {}
-for key, value in tasksLen.items():
-    percentLists[key] = round((value/totalTasks)*100)
-
-    # print(f"{percentLists[key]}% of your tasks comes from flag: {key}")
+listTasksInList(server)
 
 
-# DE QUAL LISTA VEM AS TASKS QUE EU MAIS CONCLUO
-notConcludedTasks = {}
-for key, value in tasks.items():
-    notConcludedTasks[key] = 0
-    for item in value:
-        print(f"\n {item['title']}   |  {item['status']}")
-        if item['status'] == 'needsAction':
-            notConcludedTasks[key] += 1
 
-print(notConcludedTasks)
-# print(tasks)
+
+
+
+# for flag in flagsList:
+#     response = server.tasks().list(
+#         tasklist=flag['id'],
+#         maxResults=100,
+#         showDeleted=False,
+#         showCompleted=True,
+#         # completedMax='2023-02-11T00:00:00.000Z'
+#     ).execute()
+#     items = response.get('items')
+
+#     totalTasks += len(items)
+#     # Group items in a object according to its flag 
+#     tasks[flag['title']] = items
+#     # Group items LENGTH in a object according to its flag 
+#     tasksLen[flag['title']] = len(items)
+
+# print(totalTasks)
+
+# # 1- CALCULAR PORCENTAGEM IMPACTO DA LISTA X NO TOTAL DE ATIVIDADES
+# # RETORNAR QUANTAS TAREFAS DE CADA LISTA FORAM FEITAS
+# # X% das suas atividades sao advindas da lista Y 
+# percentLists = {}
+# for key, value in tasksLen.items():
+#     percentLists[key] = round((value/totalTasks)*100)
+#     print(f"{percentLists[key]}% of your tasks comes from flag: {key}")
+
+
+# # DE QUAL LISTA VEM AS TASKS QUE EU MAIS CONCLUO
+# notConcludedTasks = {}
+# for key, value in tasks.items():
+#     notConcludedTasks[key] = 0
+#     for item in value:
+#         print(f"\n {item['title']}")
+#         if item['status'] == 'needsAction':
+#             notConcludedTasks[key] += 1
+
+# # print(notConcludedTasks)
+# # for key, value in tasks.items():
+# #     print(f"{value}\n")
